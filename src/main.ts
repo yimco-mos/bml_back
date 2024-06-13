66b6c3d67492bbd203247d0a78392691f62e99f4
Yimco Mos <117927391+yimco-mos@users.noreply.github.com>
2024-06-13 12:34:17 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   app.enableCors(corsOptions);
 
-    const port = process.env.PORT || 3000
+    const port = process.env.PORT || 4000
   await app.listen(port);
 }
 
